65d5eb3ae6b0464f46a2cf32b36d7e3a3fcf1de4
Abhishek Kumar <lit2021049@iiitl.ac.in>
2024-03-17 20:16:22 +0530

    Corrected Errors

--- app/page.js
+++ app/page.js
@@ -9,11 +9,11 @@ import HeroSection from "./(components)/landing/HeroSection";
 import Price from "./(components)/price-comparison/page";
 import ProfileForm from "./(components)/signup/page";
 import { useCallback } from "react";
-import Particles from "react-tsparticles";
+// import Particles from "react-tsparticles";
 //import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "tsparticles-slim";
+// import { loadSlim } from "tsparticles-slim";
 import { useTheme } from "next-themes";
-import StarSky from "react-star-sky";
+// import StarSky from "react-star-sky";
 import Faq from "./(components)/landing/Faq";
 import HeroSection1 from "./(components)/landing/HeroSection1";
 
@@ -25,19 +25,15 @@ export default function Home() {
     <main className="bg-slate-200 dark:bg-slate-900 text-black dark:text-slate-200">
       {/* {theme === "dark" && <BackgroundBeams />} */}
       <HeroSection />
-<<<<<<< HEAD
       <HeroSection1/>
       <Card_Home />
-      
       <Faq/>
       <AnimatedTooltipPreview />
       
-=======
      <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
      <script src="https://mediafiles.botpress.cloud/9da1583c-9e9c-45ff-908c-dd21beee74d1/webchat/config.js" defer></script>
-      <Card_Home/>
-      <AnimatedTooltipPreview/>
->>>>>>> de8eef30e0be9dc52a64c30859583ac2567cf1bf
+      {/* <Card_Home/>
+      <AnimatedTooltipPreview/> */}
     </main>
   );
 }
