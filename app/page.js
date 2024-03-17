88031209c378715e8199c5278347d9a7c6082c9b
Aryan-beast <101006432+Aryan-beast@users.noreply.github.com>
2024-03-16 23:54:40 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,6 +11,8 @@ export default function Home() {
   return (
     <main className="bg-slate-200 dark:bg-slate-900 text-black dark:text-slate-200">
       <HeroSection />
+      <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+      <script src="https://mediafiles.botpress.cloud/646a28a5-a701-4134-bf77-50c76083f2f5/webchat/config.js" defer></script>
       <Card_Home/>
       <AnimatedTooltipPreview/>
     </main>
