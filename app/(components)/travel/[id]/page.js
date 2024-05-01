3f0b90b5b633f447719bc1fe360efbe3d4b4ff69
kitish <kitishkumar2003@gmail.com>
2024-05-02 03:54:47 +0530

    checking 2

--- app/(components)/travel/[id]/page.js
+++ app/(components)/travel/[id]/page.js
@@ -8,26 +8,29 @@ export default async function Travel({ params: { id } }) {
   if (!session) {
     redirect("/login");
   }
-  const card = await db.travel.findUnique({
-    where: {
-        id: 1
-    },
-    select: {
-        id: true,
-        usersId: true,
-        from: true,
-        to: true,
-        date: true,
-        users: {
-            select: {
-                username: true,
-                name: true
-            }
-        },
-        friends: true
-    }
-  })
-  if(!card) notFound()
-  const currentUsername = session?.user?.username
-  return <Card id={card.id} usersId={card.usersId} from={card.from} to={card.to} date={card.date} username={card.users.username} name={card.users.name} friends={card.friends} currentUsername={currentUsername} />
+  return (
+    <div>{id}</div>
+  )
+  // const card = await db.travel.findUnique({
+  //   where: {
+  //       id: parseInt(id)
+  //   },
+  //   select: {
+  //       id: true,
+  //       usersId: true,
+  //       from: true,
+  //       to: true,
+  //       date: true,
+  //       users: {
+  //           select: {
+  //               username: true,
+  //               name: true
+  //           }
+  //       },
+  //       friends: true
+  //   }
+  // })
+  // if(!card) notFound()
+  // const currentUsername = session?.user?.username
+  // return <Card id={card.id} usersId={card.usersId} from={card.from} to={card.to} date={card.date} username={card.users.username} name={card.users.name} friends={card.friends} currentUsername={currentUsername} />
 }
