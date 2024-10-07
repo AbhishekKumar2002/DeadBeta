7edbcc1f40ba2cd7ac79ebc2dfd16a70defb2e9e
LIT2021015 <lit2021015@iiitl.ac.in>
2024-10-07 22:06:16 +0530

    v3.1.1

--- app/(components)/landing/AnimatedToolTipPreview.jsx
+++ app/(components)/landing/AnimatedToolTipPreview.jsx
@@ -6,37 +6,27 @@ const people = [
   {
     id: 1,
     name: "Charandeep Kumar",
-    designation: "Developed Event Creation and Pairing integrated with mail request",
-    image:
-      "/team/kitish.jpg",
+    designation:
+      "Developed Event Creation and Pairing integrated with mail request",
+    image: "/team/kitish.jpg",
   },
   {
     id: 2,
     name: "Ayush Kamal",
     designation: "Developed Chat System & Authentication",
-    image:
-      "/team/ayush.jpg",
+    image: "/team/ayush.jpg",
   },
   {
     id: 3,
     name: "Abhishek Kumar",
     designation: "Designed Frontend using shdacn, accertinity & threejs",
-    image:
-      "/team/abhishek.jpg",
+    image: "/team/abhishek.jpg",
   },
   {
     id: 4,
     name: "Shubham Pal",
     designation: "Developed price prediction & OCR",
-    image:
-      "/team/shubham.jpg",
-  },
-  {
-    id: 5,
-    name: "Aryan Nautiyal",
-    designation: "Developed Chatbot & Payment",
-    image:
-      "/team/aryan.jpg",
+    image: "/team/shubham.jpg",
   },
 ];
 
